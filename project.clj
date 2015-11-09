97b41668ff948e997e2f13d726ccd3201b79ab38
Mathieu Gauthron <mathieu@matlux.net>
2015-11-09 21:48:43 +0000

    fixed version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.quantechlab/ibclient "9.70.01"]
+                 [com.quantechlab/ibclient "9.71.01"]
                  ]
   :java-source-paths ["src/java" "test/java"]
   :source-paths ["src/clj"]
