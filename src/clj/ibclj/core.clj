c61775974e5a2bd19f9fcd511d59e508497c04a9
nabacg <nabacg@gmail.com>
2015-11-09 21:09:20 +0000

    We're connected msg from clojure

--- src/clj/ibclj/core.clj
+++ src/clj/ibclj/core.clj
@@ -17,16 +17,34 @@
 (defn create-controller []
   (reify
     com.ib.controller.ApiController$IConnectionHandler
+    (accountList [this list] (println "AccountList" list))
+    (message [this id error-code error-msg] (println id error-code error-msg))
+    (error [this e] (println e))
+    (show [this msg] (println msg))
     (connected [this] (println "We are connected!"))
     (disconnected [this] (println "We are diconnected!"))
 
     ))
 
+(defn api-ctrl []
+  (com.ib.controller.ApiController. (create-controller)
+                                    (reify com.ib.controller.ApiConnection$ILogger
+                                      (log [this s] (println s)))
+                                    (reify com.ib.controller.ApiConnection$ILogger
+                                      (log [this s] (println s)))))
+
+
+(defn start []
+  (let [api (api-ctrl)]
+    (.connect api "localhost" 7497 5 "")
+    (Thread/sleep 1000)
+    (.disconnect api)))
+
 
 (defn -main
   "the main function"
   []
-  (println "Hello, World!")
+ (start)
 
 
       )
